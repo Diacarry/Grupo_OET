b659b70fb7960039c27a695206717e38fccdd0e4
Andres Carranza <diacarry@gmail.com>
2019-10-25 11:26:47 -0500

    Creacion de las tablas 'owners', 'vehicles' y 'drivers' con sus respectivas columnas

---
+++ database/migrations/2019_10_25_161028_create_owners_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOwnersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('owners', function (Blueprint $table) {
+            $table->string('identification', 25); /* cedula */
+            $table->timestamps();
+
+            $table->string('first_name', 50); /* primer_nombre */
+            $table->string('second_name', 50); /* segundo_nombre */
+            $table->string('last_name', 100); /* apellidos */
+            $table->string('address'); /* direccion */
+            $table->string('phone'); /* telefono */
+            $table->string('city'); /* ciudad */
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('owners');
+    }
+}
---
+++ database/migrations/2019_10_25_161058_create_vehicles_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVehiclesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vehicles', function (Blueprint $table) {
+            $table->string('license_plate', 15); /* placa */
+            $table->timestamps();
+
+            $table->string('color', 25); /* color */
+            $table->string('brand'); /* marca */
+            $table->string('type'); /* tipo */
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vehicles');
+    }
+}
